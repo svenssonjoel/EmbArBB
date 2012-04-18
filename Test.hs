f8488ca52c4dd0713c8b35a9d0363e0b274c7695
Joel Svensson <svenssonjoel@yahoo.se>
2012-04-18 22:13:10 +0200

    -

--- Test.hs
+++ Test.hs
@@ -378,7 +378,7 @@ testFor =
     liftIO$ putStrLn str
   
   
-    let v1 = Vector (V.fromList [0..9::Int32]) (One 10)
+    let v1 = Vector (V.fromList [0..99999999::Int32]) (One 100000000)
     
     -- execute f 
     -- (Vector dat n,i) <- execute f v1
@@ -393,7 +393,7 @@ testFor =
       let (x,y) = for' (\(v',i)  -> i <* 10)  
                        (\(v',i) -> (v + v',i+1))
                        (v,0)
-      in (y+index1 x 4)
+      in (y+index1 x 999999)
                             
 testFor2 = 
   withArBB $  
@@ -413,7 +413,7 @@ testFor2 =
   where
     fun :: Exp Int32 -> (Exp Int32,Exp Int32)
     fun v = 
-      let (x,y) = for' (\(v',i)  -> i <* 10)  
+      let (x,y) = for' (\(v',i) -> i <* 10)  
                        (\(v',i) -> (v + v',i+1))
                        (v,0)
       in (x+1,y+1)
