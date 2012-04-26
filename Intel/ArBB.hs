9aa46d06903d4bbaa3df198b27e4dc57f758fcb2
Joel Svensson <svenssonjoel@yahoo.se>
2012-04-26 10:49:27 +0200

    added cabal file

--- Intel/ArBB.hs
+++ Intel/ArBB.hs
@@ -7,7 +7,10 @@ module Intel.ArBB (module Intel.ArBB.Syntax,
                    module Intel.ArBB.Vector,
                    module Intel.ArBB.Language,
                    module Intel.ArBB.GenArBB,
-                   module Intel.ArBB.Types) where 
+                   module Intel.ArBB.Types,
+                   module Intel.ArBB.Data,
+                   module Intel.ArBB.WithArBB,
+                   module Intel.ArBB.Data.Int) where 
 
 import Intel.ArBB.Syntax
 import Intel.ArBB.Capture 
@@ -17,3 +20,6 @@ import Intel.ArBB.Vector
 import Intel.ArBB.Language
 import Intel.ArBB.GenArBB
 import Intel.ArBB.Types 
+import Intel.ArBB.Data
+import Intel.ArBB.WithArBB 
+import Intel.ArBB.Data.Int
