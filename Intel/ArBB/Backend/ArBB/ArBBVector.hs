24f23c4bc2ad21e152e1d1393bf44896013fc290
Joel Svensson <svenssonjoel@yahoo.se>
2012-06-15 14:22:43 +0200

    added

---
+++ Intel/ArBB/Backend/ArBB/ArBBVector.hs
@@ -0,0 +1,9 @@
+module Intel.ArBB.Backend.ArBB.ArBBVector where 
+
+import Intel.ArBB.Vector
+
+
+data ArBBDVector d a = ArBBDVector { arbbDVectorID :: Integer
+                                   , arbbDVectorShape :: Dim}  
+
+
