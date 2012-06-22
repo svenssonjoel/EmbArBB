703a46a37bf5c2617eee4fce2a6888136850a3d5
Joel Svensson <svenssonjoel@yahoo.se>
2012-06-22 21:54:27 +0200

    cleaning

--- Intel/ArBB/Backend/ArBB/ArBBVector.hs
+++
@@ -1,9 +0,0 @@
-module Intel.ArBB.Backend.ArBB.ArBBVector where 
-
-import Intel.ArBB.Vector
-
-
-data ArBBDVector d a = ArBBDVector { arbbDVectorID :: Integer
-                                   , arbbDVectorShape :: Dim}  
-
-
